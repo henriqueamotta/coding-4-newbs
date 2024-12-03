f9842c6507cee870d31b6ece7a984b9c2e775b74
Beatriz Zonaro <77759777+bezoc@users.noreply.github.com>
2024-12-03 15:39:53 -0300

    fix after_action

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_173614) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
