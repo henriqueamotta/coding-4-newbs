cf8dc1c5dd8c7c7a47220001f1f562d5831d7b54
douglas teixeira silva <douglasts@outlook.com>
2024-12-03 12:36:04 -0300

    feito o merge.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
   resources :articles
-=======
 
 
   get "up" => "rails/health#show", as: :rails_health_check
