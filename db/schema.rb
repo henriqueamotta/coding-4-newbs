8e01c4df2c946f598d50f03df7ee7f54c45540dd
douglas teixeira silva <douglasts@outlook.com>
2024-12-12 14:45:54 -0300

    feito os testes para subir para o heroku

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_09_152531) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: falserails 
+    t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
